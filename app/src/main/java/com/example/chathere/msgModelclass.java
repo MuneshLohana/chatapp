ec150226e8810039b1d9bb0dc43e11f8b40e53b5
MuneshLohana <munesh.lohana@informatik.hs-fulda.de>
2024-02-20 22:00:51 +0100

    first commit

---
+++ app/src/main/java/com/example/chathere/msgModelclass.java
@@ -0,0 +1,46 @@
+package com.example.chathere;
+
+public class msgModelclass {
+    String messageId;
+    String message;
+    String senderid;
+    long timeStamp;
+
+    public msgModelclass() {
+    }
+
+    public msgModelclass(String messageId, String message, String senderid, long timeStamp) {
+        this.messageId = messageId;
+        this.message = message;
+        this.senderid = senderid;
+        this.timeStamp = timeStamp;
+    }
+
+    public String getMessageId() {
+        return messageId;
+    }
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSenderid() {
+        return senderid;
+    }
+
+    public void setSenderid(String senderid) {
+        this.senderid = senderid;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+}
